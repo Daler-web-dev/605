6f540113451dba8c810c63dc2178265287f340fc
Tima7371 <amirjonravshanov@gmail.com>
2022-02-26 21:52:49 +0500

    created repo

---
+++ index.js
@@ -0,0 +1,90 @@
+// let obj = {} // object
+// let arr = [] // object
+// let error = null // object
+
+// console.log(typeof(obj));
+
+// var user = 'a' 
+// var user = 'b' 
+
+// console.log(user);
+
+// var
+
+// **
+// %
+// == // unstrict 
+// === // strict
+// = // 
+
+// console.log(2 > 2);
+// console.log(2 <= 2);
+
+// eval()
+
+
+
+
+//  методы рат с типами данны (string)
+
+// let text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae, nemo.'
+// let name = 'alex'
+
+// text.toUpperCase()
+// text.toLowerCase()
+// text.length
+// let a = text.slice(6) // world
+// console.log(name.length);
+// name.search('l')
+
+
+// let a = 'Lex luter has a big suit'
+
+// alex
+
+
+// console.log(a.search('a'));
+
+// console.log(a.slice(a.search('a'),a.search('a') + 1) + a.slice(2,3));
+
+// console.log(b[b.length - 1] === 'index.html');
+
+// let a = 'Lorem, ipsum dolor sit, amet consectetur, adipisicing elit, Molestiae, nemo.'
+
+// let b = window.location.href.split('/')
+// a = a.replace('dolor', 'sum')
+// console.log(a);
+// a.includes('sit') // boolean
+// a.indexOf('sit')// boolean
+// console.log(a.charAt(1));
+
+
+
+// console.log(a[1]);
+
+
+// slice()
+// search()
+// toUpperCase()
+// toLowerCase()
+// name.indexOf('x')
+// name.replace('alex', 'jorge')
+// name.replace('alex', 'jorge')
+// name.charAt()
+// name.length
+
+// let value = '        Alex Adams        '
+
+// value = value.trim()
+
+// console.log(value.charAt());
+
+// let a = 'aaa@bbb@ccc'
+
+// !
+// replace()
+// =
+
+// let random = Math.random()
+
+// console.log(random);
